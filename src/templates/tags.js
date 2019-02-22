35e305570fe0ddf58f1248ac693c8ca527dcb624
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-02-22 17:34:33 +0000

    Added tags template

---
+++ src/templates/tags.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "gatsby";
+import NavLink from "../components/NavLink"
+import Layout from "../components/Layout"
+import ImageChecker from "../components/ImageChecker"
+
+const IndexPage = ({ data, pageContext }) => {
+    const { group, index, first, last, pageCount, tag } = pageContext;
+    const previousUrl = index - 1 === 1 ? "" :
+        index - 1 > 1 ?
+        "page/" + (index - 1).toString() :
+        (index - 1).toString();
+    const nextUrl = (index + 1).toString();
+    const tagName = tag.toString();
+
+    return (
+        <Layout>
+
+            <h1>Tag: {tagName}</h1>
+
+            {group.map(({ node }) => (
+                <div key={node.slug} className={"post"} style={{ marginBottom: 50 }}>
+
+                    <ImageChecker featuredMedia={node.featured_media}/>
+
+                    <Link to={node.slug}>
+                        <h3>{node.title}</h3>
+                    </Link>
+
+                    <div className={"post-content"} dangerouslySetInnerHTML={{__html: node.excerpt}} />
+
+                    {node.date}
+                </div>
+            ))}
+            <div className="previousLink">
+                <NavLink test={first} url={previousUrl} text="Go to Previous Page" />
+            </div>
+            <div className="nextLink">
+                <NavLink test={last} url={"page/" + nextUrl} text="Go to Next Page" />
+            </div>
+        </Layout>
+    );
+};
+
+export default IndexPage;
