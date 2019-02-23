e5c4f51ca20f0bcc7e2b7e5aa49212ed99d8e15a
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-02-23 11:11:43 +0000

    Added category page

---
+++ src/templates/categories.js
@@ -0,0 +1,52 @@
+import React from "react";
+import _ from "lodash";
+import { Link } from "gatsby";
+import ArchivePaginationLinks from "../components/ArchivePaginationLinks"
+import Layout from "../components/Layout"
+import ImageChecker from "../components/ImageChecker"
+
+const IndexPage = ({ data, pageContext }) => {
+    const { group, index, first, last, pageCount, additionalContext, pathPrefix } = pageContext;
+    const previousUrl = index - 1 === 1 ? "" :
+        index - 1 > 1 ?
+        "page/" + (index - 1).toString() :
+        (index - 1).toString();
+    const nextUrl = (index + 1).toString();
+
+    const currentCat = additionalContext.category;
+
+    return (
+        <Layout>
+
+            <h1>Category {currentCat}</h1>
+
+            {group.map(({ node }) => (
+                <div key={node.slug} className={"post"} style={{ marginBottom: 50 }}>
+
+                    <ImageChecker featuredMedia={node.featured_media}/>
+
+                    <Link to={node.slug}>
+                        <h3>{node.title}</h3>
+                    </Link>
+
+                    <div className={"post-content"} dangerouslySetInnerHTML={{__html: node.excerpt}} />
+
+                    {node.date}
+                </div>
+            ))}
+
+            <ArchivePaginationLinks
+                pageCount = {pageCount}
+                first = {first}
+                last = {last}
+                previousUrl = {`${pathPrefix}/${_.kebabCase(currentCat)}/${previousUrl}`}
+                nextUrl = {`${pathPrefix}/${_.kebabCase(currentCat)}/page/${nextUrl}`}
+                prevText = "Go to Previous Page"
+                nextText = "Go to Next Page"
+            />
+
+        </Layout>
+    );
+};
+
+export default IndexPage;
