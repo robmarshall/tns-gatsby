14f1355764ad3e4a90c12ed8b7d1c6a22781c97f
Rob Marshall <rob@fox.agency>
2019-03-13 16:30:51 +0000

    added styling to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,13 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styles from "./header.scss"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header className="header">
+      <div className="header__wrap">
+      <h1 className="header__title">
+        <Link className="header__link">
           {siteTitle}
         </Link>
       </h1>
