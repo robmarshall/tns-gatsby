758726c5b530c112fc1e5f498145bd9e5b8a2a7b
Rob Marshall <rob@fox.agency>
2019-03-14 11:01:51 +0000

    Added layout and header sass

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import styles from "./header.scss"
+import "./header.scss"
+import '../../utils/sass/layout/layout.scss';
 
 const Header = ({ siteTitle }) => (
   <header className="header">
-      <div className="header__wrap">
+      <div className="container header__wrap">
           <h1 className="header__title">
-              <Link className="header__link">
+              <Link className="header__link" to="/">
                   {siteTitle}
               </Link>
           </h1>
