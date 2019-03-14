30ac0c1718039454a4ee9896ff399b520844ca37
Rob Marshall <rob@fox.agency>
2019-03-14 09:41:40 +0000

    Installed fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
           },
         ],
       },
-    }
+    },
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
