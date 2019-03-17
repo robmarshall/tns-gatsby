5fa3cdccba7b66cf45cd47bdf7841eadad2e698a
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-17 19:37:05 +0000

    Added prettier to computer - reformatting

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,20 @@
-import React from "react"
-import "./footer.scss"
-import '../../utils/sass/layout/layout.scss';
+import React from 'react'
+import './footer.scss'
+import '../../utils/sass/layout/layout.scss'
 
 const Footer = ({ siteTitle }) => (
-  <footer className="footer">
-      <div className="container footer__wrap">
-          By <a href="https://www.justlikethis.co.uk" className="footer__link">
-               Robert Marshall
-          </a> & <a href="https://www.gatsbyjs.org/" className="footer__link">
+    <footer className="footer">
+        <div className="container footer__wrap">
+            By{' '}
+            <a href="https://www.justlikethis.co.uk" className="footer__link">
+                Robert Marshall
+            </a>{' '}
+            &{' '}
+            <a href="https://www.gatsbyjs.org/" className="footer__link">
                 Gatsby
-           </a>
-      </div>
-  </footer>
+            </a>
+        </div>
+    </footer>
 )
 
 export default Footer
--- src/components/RootImage/index.js
+++ src/components/RootImage/index.js
@@ -1,51 +1,52 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 /**
  * https://noahgilmore.com/blog/easy-gatsby-image-components/
  */
 
-const RootImage = (props) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        images: allFile {
-          edges {
-            node {
-              relativePath
-              name
-              childImageSharp {
-                  fluid(maxWidth: 700) {
-                      base64
-                      aspectRatio
-                      src
-                      srcSet
-                      sizes
-                  }
-              }
+const RootImage = props => (
+    <StaticQuery
+        query={graphql`
+            query {
+                images: allFile {
+                    edges {
+                        node {
+                            relativePath
+                            name
+                            childImageSharp {
+                                fluid(maxWidth: 700) {
+                                    base64
+                                    aspectRatio
+                                    src
+                                    srcSet
+                                    sizes
+                                }
+                            }
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    `}
+        `}
+        render={data => {
+            const image = data.images.edges.find(n => {
+                return n.node.relativePath.includes(props.filename)
+            })
+            if (!image) {
+                return null
+            }
 
-    render={(data) => {
-      const image = data.images.edges.find(n => {
-        return n.node.relativePath.includes(props.filename);
-      });
-      if (!image) { return null; }
-
-      const imageSizes = image.node.childImageSharp.fluid;
-      return (
-        <Img
-          alt={props.alt}
-          sizes={imageSizes}
-          className={props.className}
-        />
-      );
-    }}
-  />
+            const imageSizes = image.node.childImageSharp.fluid
+            return (
+                <Img
+                    alt={props.alt}
+                    sizes={imageSizes}
+                    className={props.className}
+                />
+            )
+        }}
+    />
 )
 
-export default RootImage;
+export default RootImage
