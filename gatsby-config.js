edf0d2fe5bd030ece6e21b2e8b3357ec4e040b11
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-23 11:13:24 +0000

    Updated Analytic settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,7 +103,7 @@ module.exports = {
             options: {
                 trackingId: process.env.GOOGLE_TRACKING_ID || '',
                 // Puts tracking script in the head instead of the body
-                head: false,
+                head: true,
                 // Setting this parameter is optional
                 anonymize: true,
                 // Setting this parameter is also optional
