385ce68535f3e23549c33f729d1c4ce15498dfd3
robmarshall <robertmarshllproductions@gmail.com>
2019-08-03 13:31:58 +0100

    Added prism for code syntax

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,118 +1,122 @@
-import React, { Component } from "react"
-import moment from 'moment';
-import Layout from "../components/Layout"
-import SEO from "../components/SEO/SEO"
-import ImageChecker from "../components/ImageChecker"
+import React, { Component } from "react";
+import moment from "moment";
+import Layout from "../components/Layout";
+import SEO from "../components/SEO/SEO";
+import ImageChecker from "../components/ImageChecker";
 import CategoryList from "../components/CategoryList";
 import TagList from "../components/TagList";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 
 class PostTemplate extends Component {
-    render() {
+  render() {
+    const post = this.props.data.wordpressPost;
+    const images = post.featured_media
+      ? post.featured_media.localFile.childImageSharp
+      : "";
 
-        const post = this.props.data.wordpressPost;
-        const images = post.featured_media ? post.featured_media.localFile.childImageSharp : '';
+    Prism.highlightAll();
 
-        return (
-            <Layout>
+    return (
+      <Layout>
+        <SEO
+          title={post.title}
+          description={post.yoast_meta.yoast_wpseo_metadesc || post.excerpt}
+          article={true}
+          image={images.facebook ? images.facebook.src : ""}
+          imageAlt={post.featured_media ? post.featured_media.alt_text : ""}
+          facebookImage={images.facebook || ""}
+          twitterImage={images.twitter ? images.twitter.src : ""}
+          publishedTime={post.date}
+          modifiedTime={post.modified}
+          tags={post.tags}
+        />
 
-                <SEO
-                    title = {post.title}
-                    description = { (post.yoast_meta.yoast_wpseo_metadesc || post.excerpt) }
-                    article = {true}
-                    image = { ( images.facebook ? images.facebook.src : '' ) }
-                    imageAlt = { post.featured_media ? post.featured_media.alt_text : '' }
-                    facebookImage = { ( images.facebook || '' ) }
-                    twitterImage = { ( images.twitter ? images.twitter.src : '' ) }
-                    publishedTime = { post.date }
-                    modifiedTime = { post.modified }
-                    tags={post.tags}
-                />
+        <article className="post">
+          <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
 
-                <article className="post">
+          <time
+            className="post__date post__date--published"
+            dateTime={moment(post.date).format("YYYY-MM-DDTHH:mm:ss+00:00")}
+          >
+            {moment(post.date).format("Do MMMM YYYY")}
+          </time>
+          <time
+            className="post__date post__date--updated screen-reader-text"
+            dateTime={moment(post.modifed).format("YYYY-MM-DDTHH:mm:ss+00:00")}
+          >
+            {moment(post.modifed).format("Do MMMM YYYY")}
+          </time>
 
-                    <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+          <CategoryList cats={post.categories} />
 
-                    <time
-                        className="post__date post__date--published"
-                        dateTime={moment(post.date).format('YYYY-MM-DDTHH:mm:ss+00:00')}
-                    >
-                        {moment(post.date).format('Do MMMM YYYY')}
-                    </time>
-                    <time
-                        className="post__date post__date--updated screen-reader-text"
-                        dateTime={moment(post.modifed).format('YYYY-MM-DDTHH:mm:ss+00:00')}
-                    >
-                        {moment(post.modifed).format('Do MMMM YYYY')}
-                    </time>
+          <ImageChecker
+            featuredMedia={post.featured_media}
+            className="post__feat-image"
+          />
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
 
-                    <CategoryList cats={post.categories}/>
-
-                    <ImageChecker featuredMedia={post.featured_media} className="post__feat-image"/>
-                    <div dangerouslySetInnerHTML={{ __html: post.content }} />
-
-                    <TagList tags={post.tags}/>
-
-                </article>
-
-            </Layout>
-        )
-    }
+          <TagList tags={post.tags} />
+        </article>
+      </Layout>
+    );
+  }
 }
 
-export default PostTemplate
+export default PostTemplate;
 
 export const postQuery = graphql`
-    query currentPostQuery($id: String!) {
-        wordpressPost(id: { eq: $id }) {
-            title
-            content
-            excerpt
-            slug
-            date(formatString:"YYYY-MM-DD, HH:mm:ss")
-            modified(formatString:"YYYY-MM-DD, HH:mm:ss")
-            tags {
-              name
+  query currentPostQuery($id: String!) {
+    wordpressPost(id: { eq: $id }) {
+      title
+      content
+      excerpt
+      slug
+      date(formatString: "YYYY-MM-DD, HH:mm:ss")
+      modified(formatString: "YYYY-MM-DD, HH:mm:ss")
+      tags {
+        name
+      }
+      categories {
+        name
+      }
+      featured_media {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 700) {
+              base64
+              aspectRatio
+              src
+              srcSet
+              sizes
             }
-            categories {
-              name
+            facebook: fixed(width: 1024, height: 512) {
+              src
+              width
+              height
             }
-            featured_media {
-                localFile {
-                    childImageSharp {
-                        fluid(maxWidth: 700) {
-                            base64
-                            aspectRatio
-                            src
-                            srcSet
-                            sizes
-                        }
-                        facebook: fixed(width: 1024, height: 512) {
-                            src
-                            width
-                            height
-                        }
-                        twitter: fixed(width: 1200, height: 630) {
-                            src
-                        }
-                    }
-                }
-                title
-                alt_text
+            twitter: fixed(width: 1200, height: 630) {
+              src
             }
-
-            yoast_meta {
-                yoast_wpseo_title
-                yoast_wpseo_metadesc
-                yoast_wpseo_meta_robots_noindex
-                yoast_wpseo_meta_robots_nofollow
-                yoast_wpseo_canonical
-                yoast_wpseo_opengraph_title
-                yoast_wpseo_opengraph_description
-                yoast_wpseo_opengraph_image
-                yoast_wpseo_twitter_title
-                yoast_wpseo_twitter_description
-                yoast_wpseo_twitter_image
-            }
+          }
         }
+        title
+        alt_text
+      }
+
+      yoast_meta {
+        yoast_wpseo_title
+        yoast_wpseo_metadesc
+        yoast_wpseo_meta_robots_noindex
+        yoast_wpseo_meta_robots_nofollow
+        yoast_wpseo_canonical
+        yoast_wpseo_opengraph_title
+        yoast_wpseo_opengraph_description
+        yoast_wpseo_opengraph_image
+        yoast_wpseo_twitter_title
+        yoast_wpseo_twitter_description
+        yoast_wpseo_twitter_image
+      }
     }
-`
+  }
+`;
