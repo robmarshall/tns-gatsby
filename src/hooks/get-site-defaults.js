b60cd23bd643fd7cefdbec9af70d9b5a59d91ca6
Robert Marshall <16778244+robmarshall@users.noreply.github.com>
2019-11-14 16:49:37 +0000

    Removed unneeded settings

--- src/hooks/get-site-defaults.js
+++ src/hooks/get-site-defaults.js
@@ -13,40 +13,14 @@ function AllSiteDefaults() {
                 }
                 site: site {
                     siteMetadata {
-                        siteName
-                        description
-                        author
-                        siteUrl
-                        facebookAppID
-                        graphQlApi
-                        customHeaders {
-                            Authorization
-                        }
-                    }
-                }
-                logoPng: file(relativePath: { eq: "logo.png" }) {
-                    childImageSharp {
-                        fluid {
-                            src
-                        }
-                    }
-                }
-                facebookImage: file(
-                    relativePath: { eq: "facebook_fallback.jpg" }
-                ) {
-                    childImageSharp {
-                        fixed(width: 1024, height: 512) {
-                            src
-                        }
-                    }
-                }
-                twitterImage: file(
-                    relativePath: { eq: "twitter_fallback.jpg" }
-                ) {
-                    childImageSharp {
-                        fixed(width: 1200, height: 630) {
-                            src
-                        }
+                      locale
+                      siteName
+                      title
+                      description
+                      author
+                      url
+                      siteUrl
+                      facebookAppID
                     }
                 }
             }
