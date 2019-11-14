34802a00b97b70bf5a28a40481d829d29b728833
Robert Marshall <16778244+robmarshall@users.noreply.github.com>
2019-11-14 17:08:56 +0000

    Added image

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import Img from "gatsby-image";
 import _ from 'lodash'
 import moment from 'moment';
 import ArchivePaginationLinks from "../components/ArchivePaginationLinks"
