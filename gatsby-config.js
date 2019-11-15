b24b0324d3f7845ce6f0e1b636dacd44b1598c52
Robert Marshall <16778244+robmarshall@users.noreply.github.com>
2019-11-15 10:19:40 +0000

    Linted

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,49 @@
-let activeEnv =
+const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
 
+// eslint-disable-next-line
 console.log(`Using environment config: '${activeEnv}'`);
 
 require("dotenv").config({
-  path: `.env.${activeEnv}`
+    path: `.env.${activeEnv}`
 });
 
 module.exports = {
-  siteMetadata: {
-    locale: `en_GB`, // Fallback
-    siteName: `T&S`, // Fallback
-    title: `Website Design and Development Inspiration`,
-    description: `Website Design and Development Inspiration`,
-    author: `@RobMarProd`,
-    url: `https://thoughtsandstuff.com`,
-    siteUrl: `https://thoughtsandstuff.com`,
-    facebookAppID: `xxx`
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`
-      }
+    siteMetadata: {
+        locale: `en_GB`, // Fallback
+        siteName: `T&S`, // Fallback
+        title: `Website Design and Development Inspiration`,
+        description: `Website Design and Development Inspiration`,
+        author: `@RobMarProd`,
+        url: `https://thoughtsandstuff.com`,
+        siteUrl: `https://thoughtsandstuff.com`,
+        facebookAppID: `xxx`
     },
-    {
-        resolve: `gatsby-source-graphql`,
-        options: {
-            // This type will contain remote schema Query type
-            typeName: `WPGraphQL`,
-            // This is field under which it's accessible
-            fieldName: `wpgraphql`,
-            // Url to query from
-            url: "http://rest.thoughtsandstuff.com/graphql",
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images/`
+            }
         },
-    },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+        {
+            resolve: `gatsby-source-graphql`,
+            options: {
+            // This type will contain remote schema Query type
+                typeName: `WPGraphQL`,
+                // This is field under which it's accessible
+                fieldName: `wpgraphql`,
+                // Url to query from
+                url: "http://rest.thoughtsandstuff.com/graphql",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
               {
                 site {
                   siteMetadata {
@@ -54,26 +55,24 @@ module.exports = {
                 }
               }
             `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allWordpressPost } }) => {
-              return allWordpressPost.edges.map(edge => {
-                return Object.assign({}, edge.node, {
-                  description: edge.node.excerpt,
-                  date: edge.node.date,
-                  url: site.siteMetadata.siteUrl + "/" + edge.node.slug,
-                  guid: site.siteMetadata.siteUrl + "/" + edge.node.slug,
-                  categories:
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allWordpressPost } }) => {
+                            return allWordpressPost.edges.map(edge => {
+                                return { ...edge.node, description: edge.node.excerpt,
+                                    date: edge.node.date,
+                                    url: `${site.siteMetadata.siteUrl  }/${  edge.node.slug}`,
+                                    guid: `${site.siteMetadata.siteUrl  }/${  edge.node.slug}`,
+                                    categories:
                     edge.node.categories.length > 0
-                      ? edge.node.categories.map(cat => {
-                          return cat.name;
+                        ? edge.node.categories.map(cat => {
+                            return cat.name;
                         })
-                      : "",
-                  custom_elements: [{ "content:encoded": edge.node.content }]
-                });
-              });
-            },
-            query: `
+                        : "",
+                                    custom_elements: [{ "content:encoded": edge.node.content }]};
+                            });
+                        },
+                        query: `
                   {
                     allWordpressPost (
                         sort: { fields: [date], order: DESC }
@@ -94,51 +93,51 @@ module.exports = {
                     }
                   }
                 `,
-            output: "/rss.xml",
-            title: "T&S RSS Feed",
-            description: "Website Design and Development Inspiration",
-            managingEditor: "Robert Marshall"
-          }
-        ]
-      }
-    },
-    `gatsby-plugin-sass`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: "gatsby-plugin-html-attributes",
-      options: {
-        lang: "en"
-      }
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Merriweather`,
-            variants: [`400`, `700`]
-          }
-        ]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_TRACKING_ID || "",
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // Enables Google Optimize using your container Id
-        optimizeId: process.env.GOOGLE_OPTIMIZE_ID || "",
-        // Enables Google Optimize Experiment ID
-        experimentId: process.env.GOOGLE_EXPERIMENT_ID || "",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: process.env.GOOGLE_VARIATION_ID || ""
-      }
-    },
-    `gatsby-plugin-netlify`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    "gatsby-plugin-offline"
-  ]
+                        output: "/rss.xml",
+                        title: "T&S RSS Feed",
+                        description: "Website Design and Development Inspiration",
+                        managingEditor: "Robert Marshall"
+                    }
+                ]
+            }
+        },
+        `gatsby-plugin-sass`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: "gatsby-plugin-html-attributes",
+            options: {
+                lang: "en"
+            }
+        },
+        {
+            resolve: `gatsby-plugin-prefetch-google-fonts`,
+            options: {
+                fonts: [
+                    {
+                        family: `Merriweather`,
+                        variants: [`400`, `700`]
+                    }
+                ]
+            }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GOOGLE_TRACKING_ID || "",
+                // Puts tracking script in the head instead of the body
+                head: true,
+                // Enables Google Optimize using your container Id
+                optimizeId: process.env.GOOGLE_OPTIMIZE_ID || "",
+                // Enables Google Optimize Experiment ID
+                experimentId: process.env.GOOGLE_EXPERIMENT_ID || "",
+                // Set Variation ID. 0 for original 1,2,3....
+                variationId: process.env.GOOGLE_VARIATION_ID || ""
+            }
+        },
+        `gatsby-plugin-netlify`,
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.app/offline
+        "gatsby-plugin-offline"
+    ]
 };
