57c80902773eb9b0b451851701e7a25824abe6b0
robmarshall <robertmarshllproductions@gmail.com>
2019-12-02 08:39:07 +0000

    Fixed yoast description formatting

--- src/components/ArticleCard/index.js
+++ src/components/ArticleCard/index.js
@@ -1,57 +1,63 @@
-import React from "react";
-import { Link } from "gatsby";
-import Img from "gatsby-image";
-import he from "he";
-import _ from "lodash";
-import moment from "moment";
+import React from 'react'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+import he from 'he'
+import _ from 'lodash'
+import moment from 'moment'
 
 const ArticleCard = ({ node }) => {
-  const { slug, featuredImage, title, date, modified, excerpt, seo } = node;
+    const { slug, featuredImage, title, date, modified, excerpt, seo } = node
 
-  const image = _.get(
-    featuredImage,
-    "imageFile.childImageSharp.image1000",
-    false
-  );
+    const image = _.get(
+        featuredImage,
+        'imageFile.childImageSharp.image1000',
+        false
+    )
 
-  const featuredAlt = _.get(featuredImage, "altText", false);
-  const featuredTitle = _.get(featuredImage, "title", false);
+    const featuredAlt = _.get(featuredImage, 'altText', false)
+    const featuredTitle = _.get(featuredImage, 'title', false)
 
-  return (
-    <div className="post">
-      <Link to={slug}>
-        <div>
-          <Img
-            className="post__feat-image"
-            fluid={image}
-            title={featuredTitle}
-            alt={featuredAlt}
-          />
+    return (
+        <div className="post">
+            <Link to={slug}>
+                <div>
+                    <Img
+                        className="post__feat-image"
+                        fluid={image}
+                        title={featuredTitle}
+                        alt={featuredAlt}
+                    />
+                </div>
+
+                <h3 className="post__title">{he.unescape(title)}</h3>
+
+                <time
+                    className="post__date post__date--published"
+                    dateTime={moment(date).format('YYYY-MM-DDTHH:mm:ss+00:00')}
+                >
+                    {moment(date).format('Do MMMM YYYY')}
+                </time>
+                <time
+                    className="post__date post__date--updated"
+                    dateTime={moment(modified).format(
+                        'YYYY-MM-DDTHH:mm:ss+00:00'
+                    )}
+                >
+                    {moment(modified).format('Do MMMM YYYY')}
+                </time>
+
+                <div
+                    className="post-content"
+                    // eslint-disable-next-line
+                    dangerouslySetInnerHTML={{
+                        __html: seo.metaDesc
+                            ? `<p>${seo.metaDesc}</p>`
+                            : excerpt,
+                    }}
+                />
+            </Link>
         </div>
+    )
+}
 
-        <h3 className="post__title">{he.unescape(title)}</h3>
-
-        <time
-          className="post__date post__date--published"
-          dateTime={moment(date).format("YYYY-MM-DDTHH:mm:ss+00:00")}
-        >
-          {moment(date).format("Do MMMM YYYY")}
-        </time>
-        <time
-          className="post__date post__date--updated"
-          dateTime={moment(modified).format("YYYY-MM-DDTHH:mm:ss+00:00")}
-        >
-          {moment(modified).format("Do MMMM YYYY")}
-        </time>
-
-        <div
-          className="post-content"
-          // eslint-disable-next-line
-          dangerouslySetInnerHTML={{ __html: seo.metaDesc || excerpt }}
-        />
-      </Link>
-    </div>
-  );
-};
-
-export default ArticleCard;
+export default ArticleCard
