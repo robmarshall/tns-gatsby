28a3603d6c11b09cb28952b87d9b1473c409c302
robmarshall <robertmarshllproductions@gmail.com>
2020-04-26 10:58:42 +0100

    Added container to 404 content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,32 @@
 import React from 'react'
 import AllCategoriesList from '../components/AllCategoriesList'
 import Layout from '../containers/Layout'
+import Container from '../containers/Container'
 import RootImage from '../components/RootImage'
 
 import './404.scss'
 
 const NotFoundPage = () => (
     <Layout>
-        <h1>Couldn&#39;t Find That Page!</h1>
-        <p>
-                        You just hit a page that doesn&#39;t exist... the
-                        sadness. It might be here sometime in the future? It
-                        might have been here sometime in the past. Unfortunatly
-                        it is not here right now :(
-        </p>
+        <Container>
+            <h1>Couldn&#39;t Find That Page!</h1>
+            <p>
+                            You just hit a page that doesn&#39;t exist... the
+                            sadness. It might be here sometime in the future? It
+                            might have been here sometime in the past. Unfortunatly
+                            it is not here right now :(
+            </p>
 
-        <RootImage
-            filename="confused-squirrel.jpg"
-            className="squirrel"
-        />
+            <RootImage
+                filename="confused-squirrel.jpg"
+                className="squirrel"
+            />
 
-        <div className="suggestion">
-            <p>Maybe these categories will help:</p>
-            <AllCategoriesList />
-        </div>
+            <div className="suggestion">
+                <p>Maybe these categories will help:</p>
+                <AllCategoriesList />
+            </div>
+        </Container>
     </Layout>
 )
 
