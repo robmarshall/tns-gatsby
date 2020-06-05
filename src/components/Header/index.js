5465710ace6dd267bd46b50f164310366b4dca4f
robmarshall <robertmarshllproductions@gmail.com>
2020-06-05 16:28:26 +0100

    Updated header/listing for reduced header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
-import Container from '../../containers/Container'
 import MainMenu from '../MainMenu'
 import './header.scss'
 
 const Header = ({ siteTitle }) => (
     <header className="header">
-        <Container>
-            <div className="header__wrap">
-                <h1 className="header__title">
-                    <Link className="header__link" to="/">
-                        {siteTitle}
-                    </Link>
-                </h1>
-                <MainMenu />
-            </div>
-        </Container>
+        <div className="header__wrap">
+            <h1 className="header__title">
+                <Link className="header__link" to="/">
+                    {siteTitle}
+                </Link>
+            </h1>
+            <MainMenu />
+        </div>
     </header>
 )
 
