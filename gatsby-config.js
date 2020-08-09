1e6fe51a2baae6c97c7f917844e69952c5b4eb74
robmarshall <robertmarshllproductions@gmail.com>
2020-08-09 18:09:48 +0100

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,17 +41,12 @@ module.exports = {
                 develop: {
                     hardCacheMediaFiles: true,
                 },
-                debug: {
-                    graphql: {
-                        writeQueriesToDisk: true,
-                    },
-                },
                 type: {
                     Post: {
                         limit:
                             process.env.NODE_ENV === `development`
                                 ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                                  5000
+                                  10
                                 : // and we don't actually need more than 5000 in production for this particular site
                                   5000,
                     },
