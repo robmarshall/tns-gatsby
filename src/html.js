8477fc818e90f26560379aa11af681e25649cabe
robmarshall <robert@invoicemaker.com>
2020-11-14 13:41:24 +0000

    Added purpleads

--- src/html.js
+++ src/html.js
@@ -41,11 +41,14 @@ export default function HTML(props) {
                     dangerouslySetInnerHTML={{ __html: body }}
                 />
                 {postBodyComponents}
-
-                {/**<script
+                <script
+                    src="https://cdn.purpleads.io/load.js?publisherId=1abdb8d096ea9fdab6647ddcf48d8375:0e55c392b103921a94bdf30892db340181915261edc20d61cf81a6ebf5938d975e7e25e031008cfe33e53c08e5113b37c5d287f71f6f81fef6b90ba5905ba056"
+                    id="purpleads-client"
+                ></script>
+                <script
                     async
                     src="https://arc.io/widget.js?WrxccT2crAmKvqcpei9Qkw"
-                />**/}
+                />
             </body>
         </html>
     )
