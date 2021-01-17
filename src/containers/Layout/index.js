d1217358f9d10313be7075a3bce7e1ad09b2754c
rob <robert@invoicemaker.com>
2021-01-17 13:50:23 +0000

    Added arc after 5 seconds

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
@@ -8,13 +8,25 @@ import SEO from '../../components/SEO/SEO'
 import './layout.scss'
 import '../../sass/base/base.scss'
 
-const Layout = ({ children }) => (
-    <div>
-        <SEO />
-        <Header siteTitle="T&S" />
-        <div className="layout">{children}</div>
-        <Footer />
-    </div>
-)
+const Layout = ({ children }) => {
+    useEffect(() => {
+        setTimeout(function () {
+            var headID = document.getElementsByTagName('head')[0]
+            var newScript = document.createElement('script')
+            newScript.type = 'text/javascript'
+            newScript.src = 'https://arc.io/widget.min.js#nkJroGnK'
+            headID.appendChild(newScript)
+        }, 5000)
+    })
+
+    return (
+        <div>
+            <SEO />
+            <Header siteTitle="T&S" />
+            <div className="layout">{children}</div>
+            <Footer />
+        </div>
+    )
+}
 
 export default Layout
